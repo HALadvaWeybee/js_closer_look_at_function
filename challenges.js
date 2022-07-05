37cffffc588468b05916e3118609f3c9411b1d89
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-07-05 19:17:06 +0530

    completed 11 videos and 1 coding challenge of javascript-closer-function

---
+++ challenges.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// coding challenge -1
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        const answer = Number(prompt(`
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        `));
+    
+        // if(question1 < 4) {
+        //     this.answers[question1]+= 1;
+        //     
+        // } else {
+        //     console.log(`answer ${question1} wouldn't make sense, right?`);
+        // }
+        typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++; 
+        console.log(this.answers);
+
+        this.displayResults();
+        this.displayResults('string');
+    },
+    displayResults(type = 'array') {
+        if(type === 'array') {
+            console.log(this.answers);
+        } else if(type === 'string') {
+            console.log(`poll result are ${this.answers.join(',')}`);
+        }
+    }
+};
+
+document.body.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll)); 
+
+poll.displayResults.call({answers: [5,2,3]});    
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string');    
+
